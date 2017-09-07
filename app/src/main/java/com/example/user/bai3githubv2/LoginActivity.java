680dd3301aac77853341b76afe7e8559384f0364
veidt1997 <15520371@gm.uit.edu.vn>
2017-09-07 19:53:43 +0700

    Rejected commit: Default

---
+++ app/src/main/java/com/example/user/bai3githubv2/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.user.bai3githubv2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by USER on 07/09/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
